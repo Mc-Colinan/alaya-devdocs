644d2c25dd1ccc25faa03cb6deb41a4c3747abac
luodahui <934803813@qq.com>
2021-03-30 19:34:21 +0800

    update documents

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -97,12 +97,12 @@ const siteConfig = {
   //外部链接
   expectUrl:{
     'en':{
-      "Alaya":"https://www.alaya.network/?lang=en",
+      "Alaya":"https://alaya.network/en",
       "Foundation":"https://latticex.foundation/home",
       "Forum":"https://forum.latticex.foundation/"
     },
     "zh-CN":{
-      "Alaya":"https://www.alaya.network/?lang=zh",
+      "Alaya":"https://alaya.network/",
       "Foundation":"https://latticex.foundation/home",
       "Forum":"https://forum.latticex.foundation/"
     }
